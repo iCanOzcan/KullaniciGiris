9bc29437fa011093c6d28a0006323f6d4ccccf9c
iCanOzcan <icanozcan96@gmail.com>
2023-03-28 22:22:59 +0300

    KullaniciGiris
    www.patika.dev deki soru icin kullanici girisi programi
    Soru icin yeterli fakat gelistirme yapilacak**

---
+++ src/KullaniciGirisi.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class KullaniciGirisi {
+
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        String userName,password,newPass;
+
+        System.out.print("Kullanici adini giriniz :");
+        userName=input.nextLine();
+        System.out.print("Parolayı giriniz :");
+        password=input.nextLine();
+
+        if(userName.equals("icanozcan96") && password.equals("caiboz96"))
+        {
+            System.out.println("Basari ile giris yaptiniz.");
+        }
+        else{
+            System.out.println("Hatali giris yaptiniz.");
+            System.out.println("Sifrenizi sifirlamak ister misiniz?   Evet icin 'E'/Hayir icin 'H' harfine basiniz. ");
+            String cevap =input.nextLine();
+            if(cevap.equals("E"))
+            {
+                 System.out.print("Yeni sifrenizi giriniz :");
+                 newPass = input.nextLine();
+                if (newPass.equals("caiboz96"))
+                {
+                    System.out.println("Sifre olusturulamadi, lütfen baska sifre giriniz.");
+                }
+                else
+                {
+                    System.out.println("Sifre olusturuldu.");
+                    password=newPass;
+                    System.out.println("yeni sifreniz :"+ password);
+                }
+            }
+            else if (cevap.equals("H"))
+            {
+                System.out.println("Lutfen tekrar deneyin !");
+
+            }
+            else {
+                System.out.println("Hatali tuslama yaptiniz.");
+            }
+
+        }
+    }
+
+}
