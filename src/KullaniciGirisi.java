27ba55d1e9d01b806e361578fce3da69bfddf2da
iCanOzcan <123066429+iCanOzcan@users.noreply.github.com>
2023-03-29 08:59:40 +0300

    Update KullaniciGirisi.java

--- src/KullaniciGirisi.java
+++ src/KullaniciGirisi.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class KullaniciGirisi {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         String userName,password,newPass;
 
